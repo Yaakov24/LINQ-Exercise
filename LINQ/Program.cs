a1343529c6df8ca4c6ab11fce15a0a0dad89019d
YaakovG <ygoldenbergyo@gmail.com>
2024-04-11 12:38:38 -0400

    did it

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string>();
+
+            names.Add("BatMan");
+            names.Add("SuperMan");
+            names.Add("SpiderMan");
+            names.Add("Hulk");
+            names.Add("Fantastic 4");
+            names.Add("Black Panther");
+            names.Add("Thor");
+            names.Add("Aqua Man");
+            names.Add("Iron Man");
+            names.Add("HawkEye");
+            names.Add("Captain America");
+            names.Add("Wolverine");
+            names.Add("Venom");
+            names.Add("Flash");
+            names.Add("Green Lantern");
+
+            var inOrderOfLength = names.OrderBy(X => X.Length);
+
+            foreach(var name in inOrderOfLength)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
